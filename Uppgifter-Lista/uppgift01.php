a7b842bf4a137fd9d241515ac32b1cf0d3b9750d
Geomasty <elias.sharro@elev.ga.ntig.se>
2022-02-08 11:00:33 +0100

    l

--- Uppgifter-Lista/uppgift01.php
+++ Uppgifter-Lista/uppgift01.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Uppgift-01</title>
 </head>
 <body>
     
@@ -19,6 +19,23 @@ $namn=filter_input(INPUT_GET, 'namn');
 echo "<h1>Valkommen ${namn}!</hl>"; 
 echo "<p>Namnet $namn innehaller ", strlen($namn), " tecken.<p>"; 
 } 
+
+//W3S - exemplar:
+echo "<br> This is the first example results: ";
+echo strpos("Hello world!", "world");
+
+echo "<br> This is the second example results: ";
+echo strrev("Hello world!");
+
+echo "<br> This is the third example results: ";
+echo str_word_count("Hello world!");
+
+echo "<br> This is the fourth example results: ";
+echo strlen("Hello world!");
+
+echo "<br> This is the fifth example results: ";
+echo str_replace("world", "Dolly", "Hello world!");
+
 ?>
 
 
