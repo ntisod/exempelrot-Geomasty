0b0bc27789d35a0bea45a96cd9c64ab7ad03a571
Geomasty <elias.sharro@elev.ga.ntig.se>
2022-02-21 10:36:16 +0100

    ö

---
+++ Uppgifter-Lista/TestUppgift01.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TestUppgift-02</title>
+</head>
+<body>
+
+<?php 
+
+
+
+$d = dir(".");
+echo "C:\wsp1\exempelrot-Geomasty\Uppgifter-Lista: " . $d->path . "\n";
+echo "<ul>";
+
+while (false !== ($entry = $d->read())) {
+   echo "<li><a href='{$entry}'>{$entry}</a></li>";
+}
+echo "</ul>";
+$d->close();
+
+
+
+?>
+
+
+</body>
+</html>
